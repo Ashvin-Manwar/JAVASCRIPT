5eff1b2861892d9fa5b71c667dd6648ac470973e
Ashvin Manwar <manwar.ashvin@gmail.com>
2022-12-14 15:22:07 +0530

    Prime Number

--- Prime.js
+++ Prime.js
@@ -1,14 +1,17 @@
 function isPrime(n){
     if(n<2){
-    return false;
+    return false
     }
-    for (let i = 2; i < n; i++) {
+    // for (let i = 2; i < n; i++) {
+        for (let i = 2; i <= Math.sqrt(n); i++){
         if (n%i===0) {
-            return false;
+            return false
         }       
     }
-    return true;
+    return true
 }
-console.log(isPrime(1));
-console.log(isPrime(5));
-console.log(isPrime(4));
+console.log(isPrime(1))// false
+console.log(isPrime(5))//true
+console.log(isPrime(4))// false
+
+//Big -O =O(n)
