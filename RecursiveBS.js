f56b72b7281c4fc5b61c4697391642f5dcccf674
Ashvin Manwar <manwar.ashvin@gmail.com>
2022-12-14 17:27:12 +0530

    Binary Search

---
+++ RecursiveBS.js
@@ -0,0 +1,8 @@
+function recursiveBinarySearch(arr,target){
+
+}
+
+console.log(recursiveBinarySearch([-5,2,4,6,10],10))//4
+console.log(recursiveBinarySearch([-5,2,4,6,10],6))//3
+console.log(recursiveBinarySearch([-5,2,4,6,10],20))//-1
+//Big-O=O(log n)
