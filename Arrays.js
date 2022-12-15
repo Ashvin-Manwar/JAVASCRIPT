3e8ddb2bbde1381d7b8720de00ac7780fc08b9bc
Ashvin Manwar <manwar.ashvin@gmail.com>
2022-12-15 18:41:46 +0530

    Array Practice

---
+++ Arrays.js
@@ -0,0 +1,31 @@
+// const arr=[1,2,3,'ashvin']
+// arr.push(4)// add end of the array o/p is 1 2 3 ashvin 4
+// arr.unshift(0)// add o to start od array o/p is 0 1 2 3 ashvin 4
+// arr.pop()// remove end element i e 4 
+// arr.shift()//remove start element i.e 0
+
+// for (const item of arr) {
+//     console.log(item)
+// }
+
+// //The concat() Method
+
+// const arr1 = ["Cecilie", "Lone"]
+// const arr2 = ["Emil", "Tobias", "Linus"]
+// const arr3 = ["Robin"]
+
+// const children = arr1.concat(arr2, arr3)
+// console.log(children)// o/p[ 'Cecilie', 'Lone', 'Emil', 'Tobias', 'Linus', 'Robin' ]
+// const arr4 = [1, 2, [3, 4]];
+// const arr5 = [[5, 6], 7, 8];
+// const arr7 = arr4.concat(arr5);
+// console.log(arr7) // 1, 2, [ 3, 4 ], [ 5, 6 ], 7, 8 ]
+
+// console.log(arr[0])//1
+// map filter reduce  slice splice
+
+// The join() Method
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let text = fruits.join(" and ");
+console.log(text)//Banana and Orange and Apple and Mango
