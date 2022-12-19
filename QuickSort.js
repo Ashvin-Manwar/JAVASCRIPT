22c47a0fa2b307d8f24cc376a0b9334dafc5051a
Ashvin Manwar <manwar.ashvin@gmail.com>
2022-12-19 15:28:20 +0530

    update

--- QuickSort.js
+++ QuickSort.js
@@ -14,4 +14,6 @@ function quickSort(arr) {
   }
   
   const arr = [8, 20, -2, 4, -6]
-  console.log(quickSort(arr)) // [-6, -2, 4, 8, 20]
+  console.log(quickSort(arr)) // [-6, -2, 4, 8, 20] 
+  //worst case-O(n^2)
+  // Avg case-O(nlogn)
